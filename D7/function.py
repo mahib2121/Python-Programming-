e965aea09a25975b12265881682fd158e77be38a
Mahib Khan <120155048+mahib2121@users.noreply.github.com>
2025-02-17 19:21:03 +0600

    Add files via upload

---
+++ D7/function.py
@@ -0,0 +1,19 @@
+##finction with parameter
+def summmer(a, b):
+    result = a + b
+    print(result)
+
+
+summmer(10, 5)
+
+
+##function with no paramenter
+def dispalySave():
+    print("Save data succesfully ")
+
+
+def displyDelete():
+    print("Data Delete ")
+
+
+dispalySave()
